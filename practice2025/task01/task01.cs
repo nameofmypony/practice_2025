293edbc9b38070fa053456c3efdd5c47f9a2e893
nameofmypony <romanbezverhnij@gmail.com>
2025-06-27 19:19:31 +0600

    task01

---
+++ practice2025/task01/task01.cs
@@ -0,0 +1,20 @@
+﻿namespace task01;
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+
+        string cleaned = new(input
+            .Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            .Select(char.ToLower)
+            .ToArray());
+
+        if (cleaned.Length == 0) return false;
+
+        for (int i = 0; i < cleaned.Length; i++)
+            if (cleaned[i] != cleaned[cleaned.Length - i - 1]) return false;
+        
+        return true;
+    }
+}
