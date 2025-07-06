d3e6a1b39e9444b2915dc13cfb6b87743c04e276
nameofmypony <romanbezverhnij@gmail.com>
2025-07-07 04:12:32 +0600

    task09

---
+++ practice2025/task09/task09.cs
@@ -0,0 +1,65 @@
+﻿namespace task09;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Set the path to .dll");
+            return;
+        }
+        
+        string path = args[0];
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("File not found");
+            return;
+        }
+        
+        var asm = Assembly.LoadFrom(path);
+
+        asm.GetTypes()
+            .ToList()
+            .ForEach(t =>
+            {
+                Console.WriteLine($"Class: {t.Name}");
+                t.GetMethods().ToList().ForEach(m =>
+                {
+                    Console.WriteLine("Method:");
+                    Console.WriteLine(m.Name);
+                    if (m.GetParameters().Length == 0) return;
+                    Console.WriteLine("Parameters:");
+                    m.GetParameters().ToList().ForEach(p =>
+                    {
+                        Console.WriteLine($"{p.ParameterType.Name} {p.Name}");
+                    });
+                });
+
+                if (t.GetCustomAttributes().Any())
+                {
+                    Console.WriteLine("Attributes:");
+                    t.GetCustomAttributes().ToList().ForEach(m => Console.WriteLine(m.GetType().Name));
+                }
+
+                if (t.GetConstructors().Length != 0)
+                {
+                    Console.WriteLine("Constructors:");
+                    t.GetConstructors().ToList().ForEach(c =>
+                    {
+                        Console.WriteLine(c.Name);
+                        if (c.GetParameters().Length == 0) return;
+                        Console.WriteLine("Parameters:");
+                        c.GetParameters().ToList().ForEach(p =>
+                        {
+                            Console.WriteLine($"{p.ParameterType.Name} {p.Name}");
+                        });
+                    });
+                }
+
+                Console.WriteLine();
+            });
+    }
+}
---
+++ practice2025/task09tests/task09tests.cs
@@ -0,0 +1,162 @@
+﻿namespace task09tests;
+using Xunit;
+using System.Diagnostics;
+
+public class DllAnalyzerTests
+{
+    [Fact]
+    public void ConsoleDllAnalyzer_HasExpectedPrint()
+    {
+        var baseDir = AppContext.BaseDirectory;
+        var exePath = Path.Combine(baseDir, "../../../../task09/bin/Debug/net9.0/task09.exe");
+        
+        var projectPath = Path.GetFullPath(Path.Combine(baseDir, "../../../../task07/bin/Debug/net9.0/task07.dll"));
+        
+        string expected = "Class: DisplayNameAttribute\r\n" +
+      "Method:\r\n" +
+      "get_DisplayName\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n" +
+      "Method:\r\n" +
+      "get_TypeId\r\n" +
+      "Method:\r\n" +
+      "Match\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "IsDefaultAttribute\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Attributes:\r\n" +
+      "NullableContextAttribute\r\n" +
+      "NullableAttribute\r\n" +
+      "AttributeUsageAttribute\r\n" +
+      "Constructors:\r\n" +
+      ".ctor\r\n" +
+      "Parameters:\r\n" +
+      "String name\r\n\r\n" +
+      "Class: VersionAttribute\r\n" +
+      "Method:\r\n" +
+      "get_Major\r\n" +
+      "Method:\r\n" +
+      "get_Minor\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n" +
+      "Method:\r\n" +
+      "get_TypeId\r\n" +
+      "Method:\r\n" +
+      "Match\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "IsDefaultAttribute\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Attributes:\r\n" +
+      "AttributeUsageAttribute\r\n" +
+      "Constructors:\r\n" +
+      ".ctor\r\n" +
+      "Parameters:\r\n" +
+      "Int32 major\r\n" +
+      "Int32 minor\r\n\r\n" +
+      "Class: SampleClass\r\n" +
+      "Method:\r\n" +
+      "TestMethod\r\n" +
+      "Method:\r\n" +
+      "get_Number\r\n" +
+      "Method:\r\n" +
+      "set_Number\r\n" +
+      "Parameters:\r\n" +
+      "String value\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n" +
+      "Attributes:\r\n" +
+      "NullableContextAttribute\r\n" +
+      "NullableAttribute\r\n" +
+      "VersionAttribute\r\n" +
+      "DisplayNameAttribute\r\n" +
+      "Constructors:\r\n" +
+      ".ctor\r\n\r\n" +
+      "Class: ReflectionHelper\r\n" +
+      "Method:\r\n" +
+      "PrintTypeInfo\r\n" +
+      "Parameters:\r\n" +
+      "Type type\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n\r\n" +
+      "Class: <>c\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n" +
+      "Attributes:\r\n" +
+      "SerializableAttribute\r\n" +
+      "CompilerGeneratedAttribute\r\n" +
+      "Constructors:\r\n" +
+      ".ctor\r\n\r\n" +
+      "Class: <>c__DisplayClass0_0\r\n" +
+      "Method:\r\n" +
+      "GetType\r\n" +
+      "Method:\r\n" +
+      "ToString\r\n" +
+      "Method:\r\n" +
+      "Equals\r\n" +
+      "Parameters:\r\n" +
+      "Object obj\r\n" +
+      "Method:\r\n" +
+      "GetHashCode\r\n" +
+      "Attributes:\r\n" +
+      "CompilerGeneratedAttribute\r\n" +
+      "Constructors:\r\n" +
+      ".ctor\r\n" +
+      "\r\n";
+        
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = exePath,
+            Arguments = $"\"{projectPath}\"",
+            RedirectStandardOutput = true,
+        };
+
+        using var process = new Process();
+        process.StartInfo = startInfo;
+        process.Start();
+        process.WaitForExit();
+
+        Assert.Equal(expected, process.StandardOutput.ReadToEnd());
+    }
+}
