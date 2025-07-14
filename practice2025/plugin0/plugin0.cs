ef0b97d51b1ebd9c42cb331e602f738d4fa05fe0
nameofmypony <romanbezverhnij@gmail.com>
2025-07-15 05:13:13 +0600

    task10

---
+++ practice2025/plugin0/plugin0.cs
@@ -0,0 +1,13 @@
+﻿namespace plugin0;
+
+public interface IPlugin
+{
+    void Execute();
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class PluginLoad(string name, params string[] dependencies) : Attribute
+{
+    public string Name { get; } = name;
+    public string[] Dependencies { get; } = dependencies;
+}
