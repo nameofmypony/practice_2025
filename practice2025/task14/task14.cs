c90a36f288b2ed2a3b231f5346fcc3293fc55ea7
nameofmypony <romanbezverhnij@gmail.com>
2025-07-16 23:22:14 +0600

    task14

---
+++ practice2025/task14/task14.cs
@@ -0,0 +1,55 @@
+﻿namespace task14;
+
+public class DefiniteIntegral
+{
+    public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+    {
+        double length = (b - a) / threadsnumber;
+        double result = 0.0;
+        object lockObj = new();
+
+        using (Barrier barrier = new(threadsnumber + 1))
+        {
+            Thread[] threads = new Thread[threadsnumber];
+
+            for (int i = 0; i < threadsnumber; i++)
+            {
+                double start = a + i * length;
+                double end = (i == threadsnumber - 1) ? b : start + length;
+
+                threads[i] = new Thread(() =>
+                {
+                    double sum = ComputePartialIntegral(start, end, function, step);
+                    lock (lockObj)
+                    {
+                        result += sum;
+                    }
+                    barrier.SignalAndWait();
+                });
+                threads[i].Start();
+            }
+
+            barrier.SignalAndWait();
+        }
+
+        return result;
+    }
+
+    private static double ComputePartialIntegral(double start, double end, Func<double, double> function, double step)
+    {
+        double sum = 0.0;
+        double current = start;
+
+        while (current < end)
+        {
+            double next = Math.Min(current + step, end);
+            double height1 = function(current);
+            double height2 = function(next);
+            double width = next - current;
+            sum += (height1 + height2) * width / 2.0;
+            current = next;
+        }
+
+        return sum;
+    }
+}
